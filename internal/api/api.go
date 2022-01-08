92c84c578bcc6a785f6ce8a60bd8890f1360c159
Pingoin <p.drente@gmx.de>
2022-01-08 22:12:06 +0100

    wechsel von routing/middleware engine mux auf echo

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,123 +1,51 @@
 package api
 
 import (
-	"embed"
-	"encoding/json"
-	"fmt"
-	"io/fs"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"strconv"
 
 	"github.com/Pingoin/pingoscope/internal/altazdriver"
 	"github.com/Pingoin/pingoscope/pkg/position"
-	"github.com/gorilla/mux"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 var altAzDriver *altazdriver.AltAzDriver
 var sensorPosition *position.Position
 
-//go:generate cp -r ../../frontend/dist frontend
-//go:embed frontend
-var frontend embed.FS
-
-type Article struct {
-	Id      string `json:"Id"`
-	Title   string `json:"Title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
-}
-
-var Articles []Article
-
 func HandleRequests(port string, altazdriverNew *altazdriver.AltAzDriver, sensPosNew *position.Position) {
 	altAzDriver = altazdriverNew
 	sensorPosition = sensPosNew
 
-	stripped, err := fs.Sub(frontend, "frontend/dist")
-	if err != nil {
-		log.Fatalln(err)
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Gzip())
+	e.Use(middleware.RemoveTrailingSlash())
+
+	// Routes
+	e.Static("/", "frontend/dist")
+	e.POST("/target", setTarget)
+	e.GET("/driver", getDriver)
+	e.GET("/sensor", getSensor)
+
+	e.Logger.Fatal(e.Start(port))
+}
+
+func getDriver(c echo.Context) error {
+	return c.JSON(http.StatusAccepted, altAzDriver.GetData())
+}
+func getSensor(c echo.Context) error {
+	return c.JSON(http.StatusAccepted, sensorPosition)
+}
+
+func setTarget(c echo.Context) error {
+	position := new(position.Position)
+	if err := c.Bind(position); err != nil {
+		return err
 	}
-
-	frontendFS := http.FileServer(http.FS(stripped))
-	Articles = []Article{
-		{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
-	}
-	myRouter := mux.NewRouter().StrictSlash(true)
-	myRouter.PathPrefix("/").Handler(frontendFS)
-	myRouter.HandleFunc("/target", setTarget).Methods("POST")
-	myRouter.HandleFunc("/driver", getDriver)
-	myRouter.HandleFunc("/sensor", getSensor)
-	myRouter.HandleFunc("/articles", returnAllArticles)
-	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
-	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
-	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
-	log.Fatal(http.ListenAndServe(port, myRouter))
-}
-
-func getDriver(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(altAzDriver.GetData())
-}
-func getSensor(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(sensorPosition)
-}
-func returnAllArticles(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnAllArticles")
-	json.NewEncoder(w).Encode(Articles)
-}
-
-func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := vars["id"]
-	u, err := strconv.ParseFloat(key, 32)
-	if err != nil {
-		panic(err)
-	}
-	altAzDriver.Azimuth.SetMaxSpeed(u)
-	for _, article := range Articles {
-		if article.Id == key {
-			json.NewEncoder(w).Encode(article)
-		}
-	}
-}
-
-func createNewArticle(w http.ResponseWriter, r *http.Request) {
-	// get the body of our POST request
-	// unmarshal this into a new Article struct
-	// append this to our Articles array.
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var article Article
-	json.Unmarshal(reqBody, &article)
-	// update our global Articles array to include
-	// our new Article
-	Articles = append(Articles, article)
-
-	json.NewEncoder(w).Encode(article)
-}
-
-func deleteArticle(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-
-	for index, article := range Articles {
-		if article.Id == id {
-			Articles = append(Articles[:index], Articles[index+1:]...)
-		}
-	}
-
-}
-
-func setTarget(w http.ResponseWriter, r *http.Request) {
-	// get the body of our POST request
-	// unmarshal this into a new Article struct
-	// append this to our Articles array.
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	var position position.Position
-	json.Unmarshal(reqBody, &position)
 	altAzDriver.Azimuth.SetTarget(float64(position.Azimuth))
-
-	fmt.Printf("bla: %v", position)
-	json.NewEncoder(w).Encode(position)
+	return c.JSON(http.StatusCreated, position)
 }
